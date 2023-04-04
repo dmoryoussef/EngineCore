3574d06457a449307baf9d89fb3af0ce9220caeb
dmoryoussef <dmoryoussef@yahoo.com>
2023-04-04 07:41:15 -0500

    updates to tilemap chunk management

--- EngineCore/InfiniteTileMapDemo.h
+++ EngineCore/InfiniteTileMapDemo.h
@@ -1,7 +1,46 @@
-class TileMapChunkController : public BaseNode
+
+
+class TileMapChunkController : public BaseNode, EventListener
 {
 private:
+	DefaultTileMap* current;
 	vector<DefaultTileMap*> Chunks;
+
+	DefaultTileMap* getChunk(Vector2 min, Vector2 max)
+	{
+		for (auto c : Chunks)
+		{
+			if (isQuadvQuad(min, max, c->getPosition(), c->getPosition() + c->getSize()))
+				return c;
+		}
+		return NULL;
+	}
+
+	DefaultTileMap* getChunk(float x, float y)
+	{
+		for (auto c : Chunks)
+		{
+			if (isPointvQuad({x, y}, c->getPosition(), c->getPosition() + c->getSize()))
+			{
+				return c;
+			}
+		}
+
+		return NULL;
+	}
+
+	void onEvent(_Event* pEvent)
+	{
+		switch (pEvent->m_eType)
+		{
+			case MOUSEWORLD_EVENT:
+			{
+				MouseWorldEvent* pMouseEvent = pEvent->get<MouseWorldEvent>();
+				current = getChunk(pMouseEvent->getWorldPosition().X, pMouseEvent->getWorldPosition().Y);
+
+			} break;
+		}
+	}
 
 public:
 	TileMapChunkController()
@@ -11,6 +50,8 @@ public:
 		Chunks.push_back(new DefaultTileMap(0, 10, 10, 10));
 		Chunks.push_back(new DefaultTileMap(10, 0, 10, 10));
 		Chunks.push_back(new DefaultTileMap(20, 0, 10, 10));
+
+		registerListener(MOUSEWORLD_EVENT);
 	};
 
 	DefaultTile* tile(int worldX, int worldY)
@@ -22,13 +63,20 @@ public:
 		return NULL;
 	}
 
+	void renderChunkUnderMouse(Render2D* pRenderer, Vector3 vCameraPosition, Vector2 vWorldMin, Vector2 vWorldMax)
+	{
+		if (current)
+			current->render(pRenderer, vCameraPosition, vWorldMin, vWorldMax);
+	}
+
 	void render(Render2D* pRenderer, Vector3 vCameraPosition, Vector2 vWorldMin, Vector2 vWorldMax)
 	{
 		//	make new list of only chunks currently in the worldview camera quad
-		for (auto c : Chunks)
+	/*	for (auto c : Chunks)
 		{
 			c->render(pRenderer, vCameraPosition, vWorldMin, vWorldMax);
-		}
+		}*/
+		renderChunkUnderMouse(pRenderer, vCameraPosition, vWorldMin, vWorldMax);
 	}
 };
 
